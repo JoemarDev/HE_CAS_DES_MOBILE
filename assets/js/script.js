028c0dda6d31d159788ef7f1b01a2383559127ea
joemardev <joemarb.dev@gmail.com>
2023-02-15 16:52:48 +0900

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
+let isLogin = false;
+
+
+const Init = () => {
+    if (isLogin) {
+        $('.guess-container').hide()
+        $('.member-container').show()
+    } else {
+        $('.guess-container').show()
+        $('.member-container').hide()
+    }
+}
+
+
 $(document).ready(() => {
     InitCarousel();
+    Init()
 });
 
 
@@ -23,6 +38,28 @@ setInterval(() => {
     console.log(DefaultValue)
     $('#odometer').html(DefaultValue)
 }, 4000)
+
+
+const CloseAllDialog = () => {
+    $('.GAME_DIALOG').hide();
+    $('.LOGIN_WRAPPER').hide();
+    $('.REGISTER_WRAPPER').hide();
+    $('.NOTE_DIALOG').hide();
+    $('.INQUIRY_DIALOG').hide();
+    $('.NOTICE_DIALOG').hide();
+    $('.EVENT_DIALOG').hide();
+    $('.CHANGE_PASSWORD_DIALOG').hide();
+    $('.HISTORY_DIALOG').hide();
+    $('.COUPON_DIALOG').hide();
+    $('.FRIEND_DIALOG').hide();
+    $('.BONUS_DIALOG').hide();
+    $('.MONEY_MOVE_DIALOG').hide();
+    $('.WITHDRAWAL_DIGALOG').hide();
+    $('.DEPOSIT_DIGALOG').hide();
+    $('.LOGIN_DIALOG').hide()
+    $('body').css("overflowY", "scroll");
+}
+
 
 
 const OpenCasinoDialog = () => {
@@ -81,4 +118,93 @@ const CloseRegisterDialog = () => {
 
 
 
+
+
+const OpenChangePasswordDialog = () => {
+    $('.CHANGE_PASSWORD_DIALOG').show();
+}
+
+const OpenHistoryDialog = () => {
+    $('.HISTORY_DIALOG').show();
+}
+
+const OpenCouponDialog = () => {
+    $('.COUPON_DIALOG').show();
+}
+
+const OpenFriendDialog = () => {
+    $('.FRIEND_DIALOG').show();
+}
+
+const OpenBonusDialog = () => {
+    $('.BONUS_DIALOG').show();
+}
+
+const OpenAccountDialog = () => {
+    $('.ACCOUNT_DIALOG').show();
+}
+
+const OpenDepositDialog = () => {
+    $('.DEPOSIT_DIGALOG').show();
+}
+
+const OpenWithdrawalDialog = () => {
+    $('.WITHDRAWAL_DIGALOG').show();
+}
+
+const OpenMoneyMoveDialog = () => {
+    $('.MONEY_MOVE_DIALOG').show();
+}
+
+const CloseAccountDialog = () => {
+    CloseAllDialog();
+    $('.ACCOUNT_DIALOG').hide();
+}
+
+const OpenNoticeDialog = () => {
+    if (!isLogin) return OpenLoginDialog()
+    $('.NOTICE_DIALOG').show();
+}
+
+const OpenNoticeContent = () => {
+    $('.notice_content').toggle();
+}
+
+const OpenEventDialog = () => {
+    if (!isLogin) return OpenLoginDialog()
+    $('.EVENT_DIALOG').show();
+}
+
+const OpenEventContent = () => {
+    $('.event_content').toggle();
+}
+
+const OpenInquiryDialog = () => {
+    if (!isLogin) return OpenLoginDialog()
+    $('.INQUIRY_DIALOG').show()
+}
+
+const WriteInquiry = () => {
+    $('.write_inquiry').show();
+    $('.inquiry_lists').hide();
+}
+
+const OpenNoteDialog = () => {
+    if (!isLogin) return OpenLoginDialog()
+    $('.NOTE_DIALOG').show()
+}
+
+
+const MemberLogin = () => {
+    isLogin = true;
+    Init();
+    CloseAllDialog()
+}
+
+const MemberLogout = () => {
+    isLogin = false;
+    Init();
+    CloseAllDialog()
+    CloseAccountDialog()
+}
 
